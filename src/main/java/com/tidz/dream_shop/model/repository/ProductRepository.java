ee15f272caf608aa341ea9917a6ea5a94ce45eed
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-09-05 15:08:13 -0300

    create ProductRepository interface

---
+++ src/main/java/com/tidz/dream_shop/model/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.tidz.dream_shop.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tidz.dream_shop.model.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
