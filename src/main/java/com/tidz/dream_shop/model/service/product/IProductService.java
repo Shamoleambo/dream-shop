4e7e89ebf592ccfbe23f8b9fc652316cefa55eb1
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-09-05 14:56:11 -0300

    create Product service interface

---
+++ src/main/java/com/tidz/dream_shop/model/service/product/IProductService.java
@@ -0,0 +1,29 @@
+package com.tidz.dream_shop.model.service.product;
+
+import java.util.List;
+
+import com.tidz.dream_shop.model.Product;
+
+public interface IProductService {
+	Product addProduct(Product product);
+
+	Product getProductById(Long id);
+
+	void deleteProductById(Long id);
+
+	void updateProduct(Product product, Long id);
+
+	List<Product> getAllProducts();
+
+	List<Product> getProductsByCategoryId(String category);
+
+	List<Product> getProductsByBrand(String name);
+
+	List<Product> getProductsByCategoryAndBrand(String category, String brand);
+
+	List<Product> getProductsByName(String name);
+
+	List<Product> getProductsByBrandAndName(String brand, String name);
+
+	Long countProductsByBrandAndName(String brand, String name);
+}
