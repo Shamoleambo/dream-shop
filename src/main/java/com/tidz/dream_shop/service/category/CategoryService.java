617fa4692cb400de3fb57788b24983e335e230d4
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-09-06 15:21:04 -0300

    implement the getCategoryById method

--- src/main/java/com/tidz/dream_shop/service/category/CategoryService.java
+++ src/main/java/com/tidz/dream_shop/service/category/CategoryService.java
@@ -1,5 +1,55 @@
 package com.tidz.dream_shop.service.category;
 
-public class CategoryService {
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.tidz.dream_shop.exception.ResourceNotFoundException;
+import com.tidz.dream_shop.model.Category;
+import com.tidz.dream_shop.repository.CategoryRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryService implements ICategoryService {
+
+	private final CategoryRepository categoryRepository;
+
+	@Override
+	public Category getCategoryById(Long id) {
+		return this.categoryRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Category not found"));
+	}
+
+	@Override
+	public Category getCategoryByName(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Category> getAllCategories() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Category addCategory(Category category) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Category updateCategory(Category category) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteCategoryById(Long id) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
---
+++ src/main/java/com/tidz/dream_shop/service/category/ICategoryService.java
@@ -0,0 +1,20 @@
+package com.tidz.dream_shop.service.category;
+
+import java.util.List;
+
+import com.tidz.dream_shop.model.Category;
+
+public interface ICategoryService {
+
+	Category getCategoryById(Long id);
+
+	Category getCategoryByName(String name);
+
+	List<Category> getAllCategories();
+
+	Category addCategory(Category category);
+
+	Category updateCategory(Category category);
+
+	void deleteCategoryById(Long id);
+}
