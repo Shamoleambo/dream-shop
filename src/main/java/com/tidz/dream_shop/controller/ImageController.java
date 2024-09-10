4dbfed14fa937ce5c4d3661ecb37c8afeb35064c
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-09-10 16:04:00 -0300

    create endpoint to download the image

--- src/main/java/com/tidz/dream_shop/controller/ImageController.java
+++ src/main/java/com/tidz/dream_shop/controller/ImageController.java
@@ -2,9 +2,16 @@ package com.tidz.dream_shop.controller;
 
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
+import java.sql.SQLException;
 import java.util.List;
 
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.tidz.dream_shop.dto.ImageDto;
+import com.tidz.dream_shop.model.Image;
 import com.tidz.dream_shop.response.ApiResponse;
 import com.tidz.dream_shop.service.image.IImageService;
 
@@ -33,6 +41,17 @@ public class ImageController {
 		} catch (Exception e) {
 			return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("Upload failed", e.getMessage()));
 		}
+	}
+
+	@GetMapping("/image/download/{imageId}")
+	public ResponseEntity<Resource> downloadImage(@PathVariable Long imageId) throws SQLException {
+		Image image = this.imageService.getImageById(imageId);
+		ByteArrayResource resource = new ByteArrayResource(
+				image.getImage().getBytes(1, (int) image.getImage().length()));
+
+		return ResponseEntity.ok().contentType(MediaType.parseMediaType(image.getFileType()))
+				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + image.getFileName() + "\"")
+				.body(resource);
 	}
 
 }
