16487523d86a046ea9eca9671d0f11d3b73fc5f4
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-09-10 16:14:03 -0300

    create the endpoint to update an image

--- src/main/java/com/tidz/dream_shop/controller/ImageController.java
+++ src/main/java/com/tidz/dream_shop/controller/ImageController.java
@@ -1,6 +1,6 @@
 package com.tidz.dream_shop.controller;
 
-import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.*;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -13,12 +13,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.tidz.dream_shop.dto.ImageDto;
+import com.tidz.dream_shop.exception.ResourceNotFoundException;
 import com.tidz.dream_shop.model.Image;
 import com.tidz.dream_shop.response.ApiResponse;
 import com.tidz.dream_shop.service.image.IImageService;
@@ -52,6 +55,23 @@ public class ImageController {
 		return ResponseEntity.ok().contentType(MediaType.parseMediaType(image.getFileType()))
 				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + image.getFileName() + "\"")
 				.body(resource);
+	}
+
+	@PutMapping("/image/{imageId}/update")
+	public ResponseEntity<ApiResponse> updateImage(@PathVariable Long imageId, @RequestBody MultipartFile file) {
+		try {
+			Image image = this.imageService.getImageById(imageId);
+			if (image != null) {
+				this.imageService.updateImage(file, imageId);
+				return ResponseEntity.ok(new ApiResponse("Update success!", null));
+			}
+
+		} catch (ResourceNotFoundException e) {
+			return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+		}
+
+		return ResponseEntity.status(INTERNAL_SERVER_ERROR)
+				.body(new ApiResponse("Update Failed", INTERNAL_SERVER_ERROR));
 	}
 
 }
