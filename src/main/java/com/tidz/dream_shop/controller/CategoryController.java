f5faccccade5ddce7f7b4743d0594fc2ed1264a3
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-09-11 10:07:54 -0300

    add endpoint to save a new Category

--- src/main/java/com/tidz/dream_shop/controller/CategoryController.java
+++ src/main/java/com/tidz/dream_shop/controller/CategoryController.java
@@ -5,9 +5,12 @@ import java.util.List;
 import static org.springframework.http.HttpStatus.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tidz.dream_shop.exception.AlreadyExistsException;
 import com.tidz.dream_shop.model.Category;
 import com.tidz.dream_shop.response.ApiResponse;
 import com.tidz.dream_shop.service.category.ICategoryService;
@@ -29,6 +32,16 @@ public class CategoryController {
 
 		} catch (Exception e) {
 			return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("Error:", INTERNAL_SERVER_ERROR));
+		}
+	}
+
+	@PostMapping("/add")
+	public ResponseEntity<ApiResponse> addCategory(@RequestBody Category name) {
+		try {
+			Category category = this.categoryService.addCategory(name);
+			return ResponseEntity.ok(new ApiResponse("Success", category));
+		} catch (AlreadyExistsException e) {
+			return ResponseEntity.status(CONFLICT).body(new ApiResponse(e.getMessage(), null));
 		}
 	}
 }
