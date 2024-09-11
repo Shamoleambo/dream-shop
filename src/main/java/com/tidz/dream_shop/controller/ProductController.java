417a292d8ee173eef15d39ddfc88ba4a6e1c66ac
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-09-11 10:48:31 -0300

    create end point to add a new Product

--- src/main/java/com/tidz/dream_shop/controller/ProductController.java
+++ src/main/java/com/tidz/dream_shop/controller/ProductController.java
@@ -4,14 +4,18 @@ import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 import java.util.List;
 
+import static org.springframework.http.HttpStatus.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tidz.dream_shop.exception.ResourceNotFoundException;
 import com.tidz.dream_shop.model.Product;
+import com.tidz.dream_shop.request.AddProductRequest;
 import com.tidz.dream_shop.response.ApiResponse;
 import com.tidz.dream_shop.service.product.IProductService;
 
@@ -37,6 +41,16 @@ public class ProductController {
 			return ResponseEntity.ok(new ApiResponse("Success", product));
 		} catch (ResourceNotFoundException e) {
 			return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+		}
+	}
+
+	@PostMapping("/add")
+	public ResponseEntity<ApiResponse> addProduct(@RequestBody AddProductRequest productRequest) {
+		try {
+			Product product = this.productService.addProduct(productRequest);
+			return ResponseEntity.ok(new ApiResponse("Success", product));
+		} catch (Exception e) {
+			return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse(e.getMessage(), null));
 		}
 	}
 
