08ddade9ce111f91806fd42a41d59f82c727fc5a
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-09-11 10:13:41 -0300

    add endpoint to get Category by id

--- src/main/java/com/tidz/dream_shop/controller/CategoryController.java
+++ src/main/java/com/tidz/dream_shop/controller/CategoryController.java
@@ -5,12 +5,14 @@ import java.util.List;
 import static org.springframework.http.HttpStatus.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tidz.dream_shop.exception.AlreadyExistsException;
+import com.tidz.dream_shop.exception.ResourceNotFoundException;
 import com.tidz.dream_shop.model.Category;
 import com.tidz.dream_shop.response.ApiResponse;
 import com.tidz.dream_shop.service.category.ICategoryService;
@@ -42,6 +44,16 @@ public class CategoryController {
 			return ResponseEntity.ok(new ApiResponse("Success", category));
 		} catch (AlreadyExistsException e) {
 			return ResponseEntity.status(CONFLICT).body(new ApiResponse(e.getMessage(), null));
+		}
+	}
+
+	@GetMapping("/category/{id}/category")
+	public ResponseEntity<ApiResponse> getCategoryById(@PathVariable("id") Long id) {
+		try {
+			Category category = this.categoryService.getCategoryById(id);
+			return ResponseEntity.ok(new ApiResponse("Found", category));
+		} catch (ResourceNotFoundException e) {
+			return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
 		}
 	}
 }
