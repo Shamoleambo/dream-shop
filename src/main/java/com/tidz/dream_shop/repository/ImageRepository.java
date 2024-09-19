40d3c30fde782ee0d1735ee9c6fcc7db96e23d9d
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-09-19 11:46:07 -0300

    add new method to find a list of images based on the product id

--- src/main/java/com/tidz/dream_shop/repository/ImageRepository.java
+++ src/main/java/com/tidz/dream_shop/repository/ImageRepository.java
@@ -1,9 +1,13 @@
 package com.tidz.dream_shop.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.tidz.dream_shop.model.Image;
+import com.tidz.dream_shop.model.Product;
 
 public interface ImageRepository extends JpaRepository<Image, Long> {
 
+	List<Image> findByProductId(Long id);
 }
