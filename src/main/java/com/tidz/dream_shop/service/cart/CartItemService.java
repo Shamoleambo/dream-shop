8b7f9a91c49e15eeb2e59cd0715edd004847f8da
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-09-19 16:01:43 -0300

    implement the method to add a cartItem to the cart

--- src/main/java/com/tidz/dream_shop/service/cart/CartItemService.java
+++ src/main/java/com/tidz/dream_shop/service/cart/CartItemService.java
@@ -1,10 +1,45 @@
 package com.tidz.dream_shop.service.cart;
 
+import org.springframework.stereotype.Service;
+
+import com.tidz.dream_shop.model.Cart;
+import com.tidz.dream_shop.model.CartItem;
+import com.tidz.dream_shop.model.Product;
+import com.tidz.dream_shop.repository.CartItemRepository;
+import com.tidz.dream_shop.repository.CartRepository;
+import com.tidz.dream_shop.service.product.IProductService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class CartItemService implements ICartItemService {
+
+	private final CartItemRepository cartItemRepository;
+	private final CartRepository cartRepository;
+	private final IProductService productService;
+	private final ICartService cartService;
 
 	@Override
 	public void addItemToCart(Long cartId, Long productId, int quantity) {
-		// TODO Auto-generated method stub
+		Cart cart = cartService.getCart(cartId);
+		Product product = productService.getProductById(productId);
+		CartItem cartItem = cart.getItems().stream().filter(item -> item.getProduct().getId().equals(productId))
+				.findFirst().orElse(null);
+
+		if (cartItem.getId() == null) {
+			cartItem.setCart(cart);
+			cartItem.setProduct(product);
+			cartItem.setQuantity(quantity);
+			cartItem.setUnitPrice(product.getPrice());
+		} else {
+			cartItem.setQuantity(cartItem.getQuantity() + quantity);
+		}
+
+		cartItem.setTotalPrice();
+		cart.addItem(cartItem);
+		cartItemRepository.save(cartItem);
+		cartRepository.save(cart);
 
 	}
 
