6c3e92d5a6f640e7106dc9c6bf0a49a840347011
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-09-19 14:34:10 -0300

    implement method to get a cart

--- src/main/java/com/tidz/dream_shop/service/cart/CartService.java
+++ src/main/java/com/tidz/dream_shop/service/cart/CartService.java
@@ -2,20 +2,32 @@ package com.tidz.dream_shop.service.cart;
 
 import java.math.BigDecimal;
 
-import com.tidz.dream_shop.model.Cart;
+import org.springframework.stereotype.Service;
 
+import com.tidz.dream_shop.exception.ResourceNotFoundException;
+import com.tidz.dream_shop.model.Cart;
+import com.tidz.dream_shop.repository.CartRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class CartService implements ICartService {
+
+	private final CartRepository cartRepository;
 
 	@Override
 	public Cart getCart(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		Cart cart = cartRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Cart not found"));
+		BigDecimal totalAmount = cart.getTotalAmount();
+		cart.setTotalAmount(totalAmount);
+		return cartRepository.save(cart);
 	}
 
 	@Override
 	public void clearCart(Long id) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
