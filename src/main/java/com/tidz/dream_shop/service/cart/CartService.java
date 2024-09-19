72034fced9b82db7d88746af915e2be432525bad
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-09-19 14:18:35 -0300

    create CartService and ICartService

---
+++ src/main/java/com/tidz/dream_shop/service/cart/CartService.java
@@ -0,0 +1,27 @@
+package com.tidz.dream_shop.service.cart;
+
+import java.math.BigDecimal;
+
+import com.tidz.dream_shop.model.Cart;
+
+public class CartService implements ICartService {
+
+	@Override
+	public Cart getCart(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void clearCart(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public BigDecimal getTotalPrice(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/com/tidz/dream_shop/service/cart/ICartService.java
@@ -0,0 +1,15 @@
+package com.tidz.dream_shop.service.cart;
+
+import java.math.BigDecimal;
+
+import com.tidz.dream_shop.model.Cart;
+
+public interface ICartService {
+
+	Cart getCart(Long id);
+
+	void clearCart(Long id);
+
+	BigDecimal getTotalPrice(Long id);
+
+}
