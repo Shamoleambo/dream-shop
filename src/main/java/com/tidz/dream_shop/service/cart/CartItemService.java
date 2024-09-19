b46db5932f6aa1f38e248e4d781066922e0960cd
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-09-19 14:53:34 -0300

    create CartItem and ICartItem interface

---
+++ src/main/java/com/tidz/dream_shop/service/cart/CartItemService.java
@@ -0,0 +1,23 @@
+package com.tidz.dream_shop.service.cart;
+
+public class CartItemService implements ICartItemService {
+
+	@Override
+	public void addItemToCart(Long cartId, Long productId, int quantity) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void removeItemFromCart(Long cartId, Long productId) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void updateItemQuantity(Long cartId, Long productId, int quantity) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/com/tidz/dream_shop/service/cart/ICartItemService.java
@@ -0,0 +1,11 @@
+package com.tidz.dream_shop.service.cart;
+
+public interface ICartItemService {
+
+	void addItemToCart(Long cartId, Long productId, int quantity);
+
+	void removeItemFromCart(Long cartId, Long productId);
+
+	void updateItemQuantity(Long cartId, Long productId, int quantity);
+
+}
