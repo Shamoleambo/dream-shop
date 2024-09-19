bc4981c629f1113a37025c88f754fe8d0b88779c
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-09-19 14:03:14 -0300

    create Cart entity

---
+++ src/main/java/com/tidz/dream_shop/model/Cart.java
@@ -0,0 +1,32 @@
+package com.tidz.dream_shop.model;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cart {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private BigDecimal totalAmount = BigDecimal.ZERO;
+
+	@OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+	private Set<CartItem> cartItems;
+}
