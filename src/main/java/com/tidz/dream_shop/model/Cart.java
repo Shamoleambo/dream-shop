9cc0a2881a5613c3bd265b64546cde41a2aae874
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-09-19 14:31:02 -0300

    create three utility methods

--- src/main/java/com/tidz/dream_shop/model/Cart.java
+++ src/main/java/com/tidz/dream_shop/model/Cart.java
@@ -28,5 +28,28 @@ public class Cart {
 	private BigDecimal totalAmount = BigDecimal.ZERO;
 
 	@OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
-	private Set<CartItem> cartItems;
+	private Set<CartItem> items;
+
+	public void addItem(CartItem item) {
+		this.items.add(item);
+		item.setCart(this);
+		updateTotalAmount();
+	}
+
+	public void removeItem(CartItem item) {
+		this.items.remove(item);
+		item.setCart(null);
+		updateTotalAmount();
+	}
+
+	private void updateTotalAmount() {
+		this.totalAmount = items.stream().map(item -> {
+			BigDecimal unitPrice = item.getUnitPrice();
+			if (unitPrice == null) {
+				return BigDecimal.ZERO;
+			}
+
+			return unitPrice.multiply(BigDecimal.valueOf(item.getQuantity()));
+		}).reduce(BigDecimal.ZERO, BigDecimal::add);
+	}
 }
