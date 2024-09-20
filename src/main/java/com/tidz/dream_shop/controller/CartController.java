423508d8351896e550f3daffb902014d45e8220f
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-09-20 10:49:36 -0300

    create end point to clear the cart

--- src/main/java/com/tidz/dream_shop/controller/CartController.java
+++ src/main/java/com/tidz/dream_shop/controller/CartController.java
@@ -2,8 +2,10 @@ package com.tidz.dream_shop.controller;
 
 import static org.springframework.http.HttpStatus.*;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +28,16 @@ public class CartController {
 		try {
 			Cart cart = cartService.getCart(cartId);
 			return ResponseEntity.ok(new ApiResponse("Success", cart));
+		} catch (ResourceNotFoundException e) {
+			return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+		}
+	}
+
+	@DeleteMapping("/{cartId}/clear")
+	public ResponseEntity<ApiResponse> clearCart(@PathVariable Long cartId) {
+		try {
+			cartService.clearCart(cartId);
+			return ResponseEntity.ok(new ApiResponse("Clear Success", null));
 		} catch (ResourceNotFoundException e) {
 			return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
 		}
