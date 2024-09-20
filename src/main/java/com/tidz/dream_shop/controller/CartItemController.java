16afb098d36c22e33b80e20eed6e92ef60a5e1f5
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-09-20 11:35:01 -0300

    generate an id for the cart in case cartId is null for testing purposes

--- src/main/java/com/tidz/dream_shop/controller/CartItemController.java
+++ src/main/java/com/tidz/dream_shop/controller/CartItemController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.tidz.dream_shop.exception.ResourceNotFoundException;
 import com.tidz.dream_shop.response.ApiResponse;
 import com.tidz.dream_shop.service.cart.ICartItemService;
+import com.tidz.dream_shop.service.cart.ICartService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -23,13 +24,20 @@ import lombok.RequiredArgsConstructor;
 public class CartItemController {
 
 	private final ICartItemService cartItemService;
+	private final ICartService cartService;
 
 	@PostMapping("/item/add")
 	public ResponseEntity<ApiResponse> addItemToCart(@RequestParam Long cartId, @RequestParam Long productId,
 			@RequestParam Integer quantity) {
 		try {
+
+			if (cartId == null) {
+				cartId = cartService.initializeNewCart();
+			}
+
 			cartItemService.addItemToCart(cartId, productId, quantity);
 			return ResponseEntity.ok(new ApiResponse("Add Item Success", null));
+
 		} catch (ResourceNotFoundException e) {
 			return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
 		}
